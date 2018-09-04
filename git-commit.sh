dd969177301c0563dd0ff714127066dedf756df5
鹏 <shaipe@sina.com>
2018-09-04 16:14:30 +0800

    Create git-commit.sh
    use script commit git change

---
+++ git-commit.sh
@@ -0,0 +1,16 @@
+cd ~/workspace/documents/
+git pull
+git add .
+
+# 提示输入提交信息
+echo -n "input commit message:"
+read input_msg
+
+# 开始提交代码
+git commit -a -m "$input_msg"
+git push
+
+echo "git commit and push success"
+
+# 添加文件后需要在终端给脚本文件执行权限
+# chmod 777 git-commit.sh 
